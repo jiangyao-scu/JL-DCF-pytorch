fa9963167009a3a49d0b59592c51b89bf8949ed8
JiangYao <56068091+jiangyao-scu@users.noreply.github.com>
2020-06-25 16:46:15 +0800

    Create resnet.py

---
+++ networks/resnet.py
@@ -0,0 +1,189 @@
+import torch.nn as nn
+import math
+import torch
+import numpy as np
+import torch.nn.functional as F
+affine_par = True
+k=64
+is_frozen = False
+# def conv3x3(in_planes, out_planes, stride=1):
+#     "3x3 convolution with padding"
+#     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+#                      padding=1, bias=False)
+
+# class BasicBlock(nn.Module):
+#     expansion = 1
+#
+#     def __init__(self, inplanes, planes, stride=1, downsample=None):
+#         super(BasicBlock, self).__init__()
+#         self.conv1 = conv3x3(inplanes, planes, stride)
+#         self.bn1 = nn.BatchNorm2d(planes, affine = affine_par)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.conv2 = conv3x3(planes, planes)
+#         self.bn2 = nn.BatchNorm2d(planes, affine = affine_par)
+#         self.downsample = downsample
+#         self.stride = stride
+#
+#     def forward(self, x):
+#         residual = x
+#
+#         out = self.conv1(x)
+#         out = self.bn1(out)
+#         out = self.relu(out)
+#
+#         out = self.conv2(out)
+#         out = self.bn2(out)
+#
+#         if self.downsample is not None:
+#             residual = self.downsample(x)
+#
+#         out += residual
+#         out = self.relu(out)
+#
+#         return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1,  dilation_ = 1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False) # change
+        self.bn1 = nn.BatchNorm2d(planes,affine = affine_par)
+        padding = 1
+        if dilation_ == 2:
+            padding = 2
+        elif dilation_ == 4:
+            padding = 4
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, # change
+                               padding=padding, bias=False, dilation = dilation_)
+        self.bn2 = nn.BatchNorm2d(planes,affine = affine_par)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4, affine = affine_par)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        # del self.fc
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+
+        self.bn1 = nn.BatchNorm2d(64,affine = affine_par)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True) # change
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation__ = 2)
+        # self.fc = nn.Linear(2048, 1000)
+
+
+    def _make_layer(self, block, planes, blocks, stride=1,dilation__ = 1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation__ == 2 or dilation__ == 4:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion,affine = affine_par),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride,dilation_=dilation__, downsample = downsample ))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes,dilation_=dilation__))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        tmp_x = []
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        tmp_x.append(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        tmp_x.append(x)
+        x = self.layer2(x)
+        tmp_x.append(x)
+        x = self.layer3(x)
+        tmp_x.append(x)
+        x = self.layer4(x)
+        tmp_x.append(x)
+
+        return tmp_x
+
+class JLModule(nn.Module):
+        def __init__(self, block, layers):
+            super(JLModule, self).__init__()
+            self.resnet = ResNet(block, layers)
+            self.relu = nn.ReLU(inplace=True)
+            self.vgg_conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(inplace=True) \
+                                           , nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+                                           nn.ReLU(inplace=True))
+            cp = []
+
+            cp.append(nn.Sequential(nn.Conv2d(64, 128, 3, 1, 1), self.relu, nn.Conv2d(128, 128, 3, 1, 1), self.relu,\
+                                    nn.Conv2d(128, k, 3, 1, 1), self.relu))
+            cp.append(nn.Sequential(nn.Conv2d(64, 128, 3, 1, 1), self.relu, nn.Conv2d(128, 128, 3, 1, 1), self.relu,\
+                                    nn.Conv2d(128, k, 3, 1, 1), self.relu))
+            cp.append(nn.Sequential(nn.Conv2d(256, 256, 5, 1, 2), self.relu, nn.Conv2d(256, 256, 5, 1, 2), self.relu,\
+                                    nn.Conv2d(256, k, 3, 1, 1), self.relu))
+            cp.append(nn.Sequential(nn.Conv2d(512, 256, 5, 1, 2), self.relu, nn.Conv2d(256, 256, 5, 1, 2), self.relu,\
+                                    nn.Conv2d(256, k, 3, 1, 1), self.relu))
+            cp.append(nn.Sequential(nn.Conv2d(1024, 512, 5, 1, 2), self.relu, nn.Conv2d(512, 512, 5, 1, 2), self.relu, \
+                                    nn.Conv2d(512, k, 3, 1, 1), self.relu))
+            cp.append(nn.Sequential(nn.Conv2d(2048, 512, 7, 1, 6, 2), self.relu, nn.Conv2d(512, 512, 7, 1, 6, 2),\
+                                    self.relu, nn.Conv2d(512, k, 3, 1, 1), self.relu))
+            self.CP = nn.ModuleList(cp)
+
+        def load_pretrained_model(self, model_path):
+            pretrained_dict = torch.load(model_path)
+            model_dict = self.resnet.state_dict()
+            pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+            model_dict.update(pretrained_dict)
+            self.resnet.load_state_dict(model_dict)
+            # self.resnet.load_state_dict(model_path, strict=True)
+            # pretrained_dict = model_path
+            # del self.resnet.fc
+            self.vgg_conv1.load_state_dict(torch.load('dataset/pretrained/vgg_conv1.pth'), strict=True)
+
+        def forward(self, x):
+# put tensor from Resnet backbone to compress model
+            feature_extract = []
+            feature_extract.append(self.CP[0](self.vgg_conv1(x)))
+            x = self.resnet(x)
+            for i in range(5):
+                feature_extract.append(self.CP[i+1](x[i]))
+            return feature_extract # list of tensor that compress model output
+
+
+def resnet101_locate():
+    model = JLModule(Bottleneck, [3, 4, 23, 3])
+    return model
