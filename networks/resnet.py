917aa3bf260c60e5bb773bf869dd45bc85367cad
JiangYao <56068091+jiangyao-scu@users.noreply.github.com>
2020-06-26 23:58:35 +0800

    Update resnet.py

--- networks/resnet.py
+++ networks/resnet.py
@@ -1,46 +1,11 @@
 import torch.nn as nn
-import math
+# import math
 import torch
 import numpy as np
 import torch.nn.functional as F
 affine_par = True
 k=64
 is_frozen = False
-# def conv3x3(in_planes, out_planes, stride=1):
-#     "3x3 convolution with padding"
-#     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-#                      padding=1, bias=False)
-
-# class BasicBlock(nn.Module):
-#     expansion = 1
-#
-#     def __init__(self, inplanes, planes, stride=1, downsample=None):
-#         super(BasicBlock, self).__init__()
-#         self.conv1 = conv3x3(inplanes, planes, stride)
-#         self.bn1 = nn.BatchNorm2d(planes, affine = affine_par)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.conv2 = conv3x3(planes, planes)
-#         self.bn2 = nn.BatchNorm2d(planes, affine = affine_par)
-#         self.downsample = downsample
-#         self.stride = stride
-#
-#     def forward(self, x):
-#         residual = x
-#
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.relu(out)
-#
-#         out = self.conv2(out)
-#         out = self.bn2(out)
-#
-#         if self.downsample is not None:
-#             residual = self.downsample(x)
-#
-#         out += residual
-#         out = self.relu(out)
-#
-#         return out
 
 
 class Bottleneck(nn.Module):
@@ -101,7 +66,6 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation__ = 2)
-        # self.fc = nn.Linear(2048, 1000)
 
 
     def _make_layer(self, block, planes, blocks, stride=1,dilation__ = 1):
@@ -169,10 +133,7 @@ class JLModule(nn.Module):
             pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
             model_dict.update(pretrained_dict)
             self.resnet.load_state_dict(model_dict)
-            # self.resnet.load_state_dict(model_path, strict=True)
-            # pretrained_dict = model_path
-            # del self.resnet.fc
-            self.vgg_conv1.load_state_dict(torch.load('dataset/pretrained/vgg_conv1.pth'), strict=True)
+            self.vgg_conv1.load_state_dict(torch.load('pretrained/vgg_conv1.pth'), strict=True)
 
         def forward(self, x):
 # put tensor from Resnet backbone to compress model
